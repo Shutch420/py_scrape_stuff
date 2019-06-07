92e70db12e10a73fd459b01adb5c1861224315d3
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-06-07 10:26:37 +0200

    api seems to have changed

--- gen01.lisp
+++ gen01.lisp
@@ -10,6 +10,7 @@
 		   html
 		   json
 		   logging
+		   requests
 		   urllib
 		   ))
 	 "from bs4 import BeautifulSoup"
@@ -25,6 +26,8 @@
 		     (action (string "find"))
 		     (maxPrice)
 		     (minPrice))))
+
+	    (string "# https://github.com/shlnz/kleinanzeigen_scraper/blob/master/provider/kleinanzeigen_ebay.py")
 	    `(def gen_url (&key ,@(loop for e in l collect
 					(destructuring-bind (name &optional value) e
 					  (if value
@@ -39,6 +42,42 @@
 					   (destructuring-bind (name &optional value) e
 					     `(urllib.parse.quote ,name))))))
 	       ))
-	 (print (gen_url :keywords (string "vega 56")))
+	 (do0
+	  "# load the url"
+	  (setf url (gen_url :keywords (string "vega 56")))
+	  (setf r (requests.get url)
+		content (r.text.replace (string "&#8203") (string ""))
+		soup (BeautifulSoup content (string "html.parser"))
+		articles (soup.find_all (string "article")
+					(dict ((string "class") (string "aditem")))))
+	  (if "articles is None"
+	      (do0
+	       (logging.info (string "No articles match."))
+					;(return None)
+	       )))
+	 (do0
+	  "# parse articles"
+	  (setf res (list))
+	  (for (article articles)
+	       (setf details (article.find (string "section") (dict ((string "class")
+								     (string "additem-details"))))
+		     price (dot (details.find (string "strong"))
+				text)
+		     vb (in (string "VB") price)
+					;(ntuple zip_code city owner_distance)
+		     header (article.find (string "h2")
+					  (dict ((string "class")
+						 (string "text-module-begin"))))
+		     href (aref (header.find (string "a")
+					     :href True)
+				(string "href"))
+					;description
+		     ignore False
+		     )
+	       ,@(let ((l `(details price vb header href ignore)))
+		   `(res.append (dict ,@(loop for e in l collect
+					     `((string ,e) ,e))))))
+	  (setf df (pd.DataFrame res)))
+	 
 	 )))
   (write-source "/home/martin/stage/py_scrape_stuff/source/run_01_scrape" code))
--- source/run_01_scrape.py
+++ source/run_01_scrape.py
@@ -4,8 +4,28 @@ import pathlib
 import html
 import json
 import logging
-from bs3 import BeautifulSoup
-
-
-def gen_url(keywords=None, categoryId=None, locationStr=None, locationId=None, radius=None, sortingField="SORTING_DATE", adType=None, posterType=None, pageNum=1, action="find", maxPrice=None, minPrice=None):
-    return "https://www.ebay-kleinanzeigen.de/s-suchanfrage.html?&keywords={}&categoryId={}&locationStr={}&locationId={}&radius={}&sortingField={}&adType={}&posterType={}&pageNum={}&action={}&maxPrice={}&minPrice={}".format(keywords, categoryId, locationStr, locationId, radius, sortingField, adType, posterType, pageNum, action, maxPrice, minPrice)
+import requests
+import urllib
+from bs4 import BeautifulSoup
+def gen_url(keywords="", categoryId="", locationStr="", locationId="", radius="", sortingField="SORTING_DATE", adType="", posterType="", pageNum="1", action="find", maxPrice="", minPrice=""):
+    return "https://www.ebay-kleinanzeigen.de/s-suchanfrage.html?&keywords={}&categoryId={}&locationStr={}&locationId={}&radius={}&sortingField={}&adType={}&posterType={}&pageNum={}&action={}&maxPrice={}&minPrice={}".format(urllib.parse.quote(keywords), urllib.parse.quote(categoryId), urllib.parse.quote(locationStr), urllib.parse.quote(locationId), urllib.parse.quote(radius), urllib.parse.quote(sortingField), urllib.parse.quote(adType), urllib.parse.quote(posterType), urllib.parse.quote(pageNum), urllib.parse.quote(action), urllib.parse.quote(maxPrice), urllib.parse.quote(minPrice))
+# load the url
+url=gen_url(keywords="vega 56")
+r=requests.get(url)
+content=r.text.replace("&#8203", "")
+soup=BeautifulSoup(content, "html.parser")
+articles=soup.find_all("article", {("class"):("aditem")})
+if ( articles is None ):
+    logging.info("No articles match.")
+# parse articles
+res=[]
+for article in articles:
+    details=article.find("section", {("class"):("additem-details")})
+    price=details.find("strong").text
+    vb=("VB" in price)
+    header=article.find("h2", {("class"):("text-module-begin")})
+    href=header.find("a", href=True)["href"]
+    ignore=False
+    res.append
+    {("details"):(details),("price"):(price),("vb"):(vb),("header"):(header),("href"):(href),("ignore"):(ignore)}
+df=pd.DataFrame(res)
