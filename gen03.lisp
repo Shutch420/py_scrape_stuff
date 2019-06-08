49df35b5161c8fd0ea43005f0d41b924a924d797
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-06-09 00:27:24 +0200

    get first level of techpowerup gpu data (i will have to follow links
    for tflops)

--- gen03.lisp
+++ gen03.lisp
@@ -36,32 +36,41 @@
 					   (destructuring-bind (name &optional value) e
 					     `(urllib.parse.quote ,name))))))
 	       ))
-	 (do0
-	  (setf url (gen_url))
-
-	  (print (dot (string "requesting {}")
-		      (format url)))
-	  (setf r (requests.get url)
-		content (r.text.replace (string "&#8203") (string ""))
-		soup (BeautifulSoup content (string "html.parser"))
-		table (soup.find (string "table") (dict ((string "class")
-							 (string "processors"))))
-		head (table.find (string "thead") (dict ((string "class")
-							 (string "colheader"))))
-		columns ("list" (map (lambda (x) (x.text.strip)) (head.find_all (string "th"))))
-		rows (dot table
-			  (find_all (string "tr")))
-		data ("list" (map (lambda (row)
-				    ("dict" (zip
-				      columns
-				      (map (lambda (td)
-					     (td.text.strip))
-					   (row.find_all (string "td"))))))
-				  (aref rows "2:") ;; top two rows are no data
- 				  ))
-		;data1 (functools.reduce operator.iconcat data (list))
-		df (pd.DataFrame data)
-		)
-	  )
+	 (setf data (list))
+	 (for (mfgr (list (string "NVIDIA")
+			  (string "AMD")))
+	  (for (year (range 2008 (+ 1 2019)))
+	       (do0
+		(setf url (gen_url :mfgr mfgr :released (str year)))
+		(time.sleep 3)
+		(print (dot (string "requesting {}")
+			    (format url)))
+		(setf r (requests.get url)
+		      content (r.text.replace (string "&#8203") (string ""))
+		      soup (BeautifulSoup content (string "html.parser"))
+		      table (soup.find (string "table") (dict ((string "class")
+							       (string "processors"))))
+		      head (table.find (string "thead") (dict ((string "class")
+							       (string "colheader"))))
+		      columns ("list" (map (lambda (x) (x.text.strip)) (head.find_all (string "th"))))
+		      rows (dot table
+				(find_all (string "tr")))
+		      data (+ data ("list" (map (lambda (row)
+						  ("dict" (+ (list (tuple (string "time") (time.time))
+								   (tuple (string "mfgr") mfgr)
+								   (tuple (string "year") year))
+							     ("list"
+							      (zip
+							     columns
+							     (map (lambda (td)
+								    (td.text.strip))
+								  (row.find_all (string "td"))))))))
+						(aref rows "2:") ;; top two rows are no data
+ 						)))
+					;data1 (functools.reduce operator.iconcat data (list))
+		      df (pd.DataFrame data)
+		      )
+		(df.to_csv (string "techpowerup_gpu.csv"))
+		)))
 	 )))
   (write-source "/home/martin/stage/py_scrape_stuff/source/run_03_scrape_techpowerup" code))
--- source/run_03_scrape_techpowerup.py
+++ source/run_03_scrape_techpowerup.py
@@ -12,14 +12,19 @@ import operator
 from bs4 import BeautifulSoup
 def gen_url(mfgr="NVIDIA", released="2008", sort="name"):
     return "https://www.techpowerup.com/gpu-specs/?&mfgr={}&released={}&sort={}".format(urllib.parse.quote(mfgr), urllib.parse.quote(released), urllib.parse.quote(sort))
-url=gen_url()
-print("requesting {}".format(url))
-r=requests.get(url)
-content=r.text.replace("&#8203", "")
-soup=BeautifulSoup(content, "html.parser")
-table=soup.find("table", {("class"):("processors")})
-head=table.find("thead", {("class"):("colheader")})
-columns=list(map(lambda x: x.text.strip(), head.find_all("th")))
-rows=table.find_all("tr")
-data=list(map(lambda row: dict(zip(columns, map(lambda td: td.text.strip(), row.find_all("td")))), rows[2:]))
-df=pd.DataFrame(data)
+data=[]
+for mfgr in ["NVIDIA", "AMD"]:
+    for year in range(2008, ((1)+(2019))):
+        url=gen_url(mfgr=mfgr, released=str(year))
+        time.sleep(3)
+        print("requesting {}".format(url))
+        r=requests.get(url)
+        content=r.text.replace("&#8203", "")
+        soup=BeautifulSoup(content, "html.parser")
+        table=soup.find("table", {("class"):("processors")})
+        head=table.find("thead", {("class"):("colheader")})
+        columns=list(map(lambda x: x.text.strip(), head.find_all("th")))
+        rows=table.find_all("tr")
+        data=((data)+(list(map(lambda row: dict((([("time",time.time(),), ("mfgr",mfgr,), ("year",year,)])+(list(zip(columns, map(lambda td: td.text.strip(), row.find_all("td"))))))), rows[2:]))))
+        df=pd.DataFrame(data)
+        df.to_csv("techpowerup_gpu.csv")
