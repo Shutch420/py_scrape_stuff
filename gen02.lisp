f7fe656d89624fff64d7188fcff20f09f69030dd
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-06-08 14:05:12 +0200

    sort by price and print table

--- gen02.lisp
+++ gen02.lisp
@@ -7,6 +7,38 @@
 		   (pd pandas)
 		   pathlib
 		   )) 
-	 (setf df (pd.read_csv (string "output_20190608b.csv")))	 
+	 (setf df (pd.read_csv (string "output_20190608b.csv")))
+
+	 (def parse_price (row)
+	   (try
+	    (do0
+	     (setf p (dot (aref row (string "price"))
+			  (strip)))
+	     (if (== (string "VB") p)
+		 (return 0d0))
+	     (return 
+	       (float (dot p
+			   (aref (split (string "\\u20ac")) 0)
+			   ))))
+	    ("Exception as e"
+	     (return 0d0))))
+	 (setf (aref df (string "price_eur"))
+	       (df.apply parse_price :axis 1)
+	       (aref df (string "link_name"))
+	       (df.apply (lambda (row)
+			   (dot (string "/")
+				(join
+				 (dot (aref row (string "href"))
+				      (aref (split (string "/"))
+					    "2:")
+				      ))
+				))
+			 :axis 1))
+	 (setf df1 (dot (df.set_index (string "price_eur"))
+			(sort_index)))
+	 (with (pd.option_context (string "display.max_rows") None
+                                  (string "display.max_columns") None)
+               (print (aref df1 (list (string "link_name")))))
+
 	 )))
   (write-source "/home/martin/stage/py_scrape_stuff/source/run_02_load" code))
--- source/run_02_load.py
+++ source/run_02_load.py
@@ -1,3 +1,16 @@
 import pandas as pd
 import pathlib
 df=pd.read_csv("output_20190608b.csv")
+def parse_price(row):
+    try:
+        p=row["price"].strip()
+        if ( (("VB")==(p)) ):
+            return (0.0e+0)
+        return float(p.split("\u20ac")[0])
+    except Exception as e:
+        return (0.0e+0)
+df["price_eur"]=df.apply(parse_price, axis=1)
+df["link_name"]=df.apply(lambda row: "/".join(row["href"].split("/")[2:]), axis=1)
+df1=df.set_index("price_eur").sort_index()
+with pd.option_context("display.max_rows", None, "display.max_columns", None):
+    print(df1[["link_name"]])
