5ce064fe9829e8ad195077ab1d87129717672ca0
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-06-09 00:55:13 +0200

    also try to get cpus
    requesting https://www.techpowerup.com/gpu-specs/?&mfgr=Intel&released=2009&sort=name
    Traceback (most recent call last):
      File "run_03_scrape_techpowerup.py", line 31, in <module>
        columns=list(map(lambda x: x.text.strip(), head.find_all("th")))
    AttributeError: 'NoneType' object has no attribute 'find_all'

--- gen03.lisp
+++ gen03.lisp
@@ -20,9 +20,11 @@
 	 ,(let ((l `((mfgr (string "NVIDIA"))
 		     (released (string "2008"))
 		     (sort (string "name"))
+		     
 		     )))
 
-	    `(def gen_url (&key ,@(loop for e in l collect
+	    `(def gen_url (&key (database (string "gpu-spec")) ;; or cpudb
+				,@(loop for e in l collect
 					(destructuring-bind (name &optional value) e
 					  (if value
 					      `(,name ,value)
@@ -36,51 +38,60 @@
 					   (destructuring-bind (name &optional value) e
 					     `(urllib.parse.quote ,name))))))
 	       ))
-	 (setf data (list))
-	 (for (mfgr (list (string "NVIDIA")
-			  (string "AMD")))
-	  (for (year (range 2008 (+ 1 2019)))
-	       (do0
-		(setf url (gen_url :mfgr mfgr :released (str year)))
-		(time.sleep 3)
-		(print (dot (string "requesting {}")
-			    (format url)))
-		(setf r (requests.get url)
-		      content (r.text.replace (string "&#8203") (string ""))
-		      soup (BeautifulSoup content (string "html.parser"))
-		      table (soup.find (string "table") (dict ((string "class")
-							       (string "processors"))))
-		      head (table.find (string "thead") (dict ((string "class")
-							       (string "colheader"))))
-		      columns ("list" (map (lambda (x) (x.text.strip)) (head.find_all (string "th"))))
-		      rows (dot table
-				(find_all (string "tr")))
-		      data (+ data ("list" (map (lambda (row)
-						  ("dict" (+ (list (tuple (string "time") (time.time))
-								   (tuple (string "mfgr") mfgr)
-								   (tuple (string "year") year)
-								   (tuple (string "url") ;;(rows[12].find('td',{"class":"vendor-NVIDIA"})).a['href']
+	 
+	 
+	 (for (database (list (string "cpudb")
+			      (string "gpu-spec")))
+	      (setf data (list))
+	      (if (== database (string "cpudb"))
+		  (setf mfgrs (list (string "Intel")
+				    (string "AMD")))
+		  (setf mfgrs (list (string "NVIDIA")
+				    (string "AMD"))))
+	      (for (mfgr mfgrs)
+	       (for (year (range 2006 (+ 1 2019)))
+		    (do0
+		     (setf url (gen_url :mfgr mfgr :released (str year)))
+		     (time.sleep 3)
+		     (print (dot (string "requesting {}")
+				 (format url)))
+		     (setf r (requests.get url)
+			   content (r.text.replace (string "&#8203") (string ""))
+			   soup (BeautifulSoup content (string "html.parser"))
+			   table (soup.find (string "table") (dict ((string "class")
+								    (string "processors"))))
+			   head (table.find (string "thead") (dict ((string "class")
+								    (string "colheader"))))
+			   columns ("list" (map (lambda (x) (x.text.strip)) (head.find_all (string "th"))))
+			   rows (dot table
+				     (find_all (string "tr")))
+			   data (+ data ("list" (map (lambda (row)
+						       ("dict" (+ (list (tuple (string "time") (time.time))
+									(tuple (string "mfgr") mfgr)
+									(tuple (string "year") year)
+									(tuple (string "url") ;;(rows[12].find('td',{"class":"vendor-NVIDIA"})).a['href']
 
-									  (dot
-									   row
-									   (find (string "td")
-										 (dict ((string "class")
-											(+ (string "vendor-") mfgr))))
-									   (aref a #+nil (find (string "a")
-										       :href True)
-										 (string "href")))))
-							     ("list"
-							      (zip
-							     columns
-							     (map (lambda (td)
-								    (td.text.strip))
-								  (row.find_all (string "td"))))))))
-						(aref rows "2:") ;; top two rows are no data
- 						)))
+									       (dot
+										row
+										(find (string "td")
+										      (dict ((string "class")
+											     (+ (string "vendor-") mfgr))))
+										(aref a #+nil (find (string "a")
+												    :href True)
+										      (string "href")))))
+								  ("list"
+								   (zip
+								    columns
+								    (map (lambda (td)
+									   (td.text.strip))
+									 (row.find_all (string "td"))))))))
+						     (aref rows "2:") ;; top two rows are no data
+ 						     )))
 					;data1 (functools.reduce operator.iconcat data (list))
-		      df (pd.DataFrame data)
-		      )
-		(df.to_csv (string "techpowerup_gpu_b.csv"))
-		)))
+			   df (pd.DataFrame data)
+			   )
+		     (df.to_csv (dot (string "techpowerup_{}.csv")
+				     (format database)))
+		     ))))
 	 )))
   (write-source "/home/martin/stage/py_scrape_stuff/source/run_03_scrape_techpowerup" code))
--- source/run_03_scrape_techpowerup.py
+++ source/run_03_scrape_techpowerup.py
@@ -10,21 +10,26 @@ import time
 import functools
 import operator
 from bs4 import BeautifulSoup
-def gen_url(mfgr="NVIDIA", released="2008", sort="name"):
+def gen_url(database="gpu-spec", mfgr="NVIDIA", released="2008", sort="name"):
     return "https://www.techpowerup.com/gpu-specs/?&mfgr={}&released={}&sort={}".format(urllib.parse.quote(mfgr), urllib.parse.quote(released), urllib.parse.quote(sort))
-data=[]
-for mfgr in ["NVIDIA", "AMD"]:
-    for year in range(2008, ((1)+(2019))):
-        url=gen_url(mfgr=mfgr, released=str(year))
-        time.sleep(3)
-        print("requesting {}".format(url))
-        r=requests.get(url)
-        content=r.text.replace("&#8203", "")
-        soup=BeautifulSoup(content, "html.parser")
-        table=soup.find("table", {("class"):("processors")})
-        head=table.find("thead", {("class"):("colheader")})
-        columns=list(map(lambda x: x.text.strip(), head.find_all("th")))
-        rows=table.find_all("tr")
-        data=((data)+(list(map(lambda row: dict((([("time",time.time(),), ("mfgr",mfgr,), ("year",year,), ("url",row.find("td", {("class"):((("vendor-")+(mfgr)))}).a["href"],)])+(list(zip(columns, map(lambda td: td.text.strip(), row.find_all("td"))))))), rows[2:]))))
-        df=pd.DataFrame(data)
-        df.to_csv("techpowerup_gpu_b.csv")
+for database in ["cpudb", "gpu-spec"]:
+    data=[]
+    if ( ((database)==("cpudb")) ):
+        mfgrs=["Intel", "AMD"]
+    else:
+        mfgrs=["NVIDIA", "AMD"]
+    for mfgr in mfgrs:
+        for year in range(2006, ((1)+(2019))):
+            url=gen_url(mfgr=mfgr, released=str(year))
+            time.sleep(3)
+            print("requesting {}".format(url))
+            r=requests.get(url)
+            content=r.text.replace("&#8203", "")
+            soup=BeautifulSoup(content, "html.parser")
+            table=soup.find("table", {("class"):("processors")})
+            head=table.find("thead", {("class"):("colheader")})
+            columns=list(map(lambda x: x.text.strip(), head.find_all("th")))
+            rows=table.find_all("tr")
+            data=((data)+(list(map(lambda row: dict((([("time",time.time(),), ("mfgr",mfgr,), ("year",year,), ("url",row.find("td", {("class"):((("vendor-")+(mfgr)))}).a["href"],)])+(list(zip(columns, map(lambda td: td.text.strip(), row.find_all("td"))))))), rows[2:]))))
+            df=pd.DataFrame(data)
+            df.to_csv("techpowerup_{}.csv".format(database))
