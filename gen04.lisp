1b1854f25290dccfe06ce803d5d41b73512a8979
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-06-09 13:00:29 +0200

    bs4 selects to many sections
    ['Graphics Processor', 'Graphics Processor_2', 'Graphics Card', 'Relative Performance', 'Clock Speeds', 'Memory', 'Render Config', 'Theoretical Performance', 'Board Design', 'Graphics Features', 'Card Notes', 'Retail boards based on this design (1)']

--- gen04.lisp
+++ gen04.lisp
@@ -23,27 +23,31 @@
 	 ;(setf row (aref df.iloc -1))
 	 (setf res (list))
 	 (for ((ntuple idx row)
-	       #-nil (df.iterrows)
-	       #+nil(list (tuple 0 (aref df.iloc -1))
-			  (tuple 1 (aref df.iloc -2))))
+	       #+nil (df.iterrows)
+	       #-nil(list ;(tuple 0 (aref df.iloc -1))
+			  (tuple 1 (aref df.iloc 347))))
 	  (do0
 	   (setf url (+ (string "https://www.techpowerup.com") row.url))
-	   (time.sleep 3)
+	   (time.sleep .2)
 	   (print (dot (string "requesting {} [{}/{}]")
 		       (format url idx (len df))))
 	   "# https://pythonprogramminglanguage.com/web-scraping-with-pandas-and-beautifulsoup/"
 	   (setf r (requests.get url)
 		 content (r.text.replace (string "&#8203") (string ""))
 		 soup (BeautifulSoup content (string "html.parser"))
-		 sections (soup.find_all (string "section")
-					 (dict ((string "class")
-						(string "details"))))
-		 detail_list (+
+		 sections (soup.find_all (lambda (tag)
+					   (and (== tag.name (string "section"))
+						(== (tag.get (string "class"))
+						    (list (string "details"))))))
+		 
+		 
+		 detail_list_ (+
 			      (list (tuple (string url) row.url)
 				    (tuple (string time) (time.time)))
 			      (functools.reduce operator.iconcat 
 			       ("list"
 				(map (lambda (section) ("list" (map (lambda (row)
+								      
 								      (tuple (+ (dot (section.find (string "h2"))
 										     text
 										     (strip))
@@ -56,7 +60,20 @@
 								     (list)))))
 				     sections))
 			       (list))))
-	   (res.append ("dict" detail_list))
+	   (do0
+	    (setf columns (list))
+	    (for (section sections)
+		 (setf col_orig (section.h2.text.strip)
+		       col col_orig
+		       count 2)
+		 ;; append number if col occured already
+		 (while (in col columns)
+		   (setf col (+ col_orig (string "_") (str count))
+			 count (+ 1 count)))
+		 (columns.append col)))
+	   (print columns)
+	   #+nil (res.append ("dict" detail_list))
 	   (setf df_out (pd.DataFrame res))
-	   (df_out.to_csv (string "techpowerup_gpu-specs_details.csv")))))))
+	   (df_out.to_csv (dot (string "techpowerup_gpu-specs_details_{}.csv")
+			       (format (int (time.time))))))))))
   (write-source "/home/martin/stage/py_scrape_stuff/source/run_04_scrape_techpowerup_details" code))
--- source/run_04_scrape_techpowerup_details.py
+++ source/run_04_scrape_techpowerup_details.py
@@ -12,16 +12,25 @@ import operator
 from bs4 import BeautifulSoup
 df=pd.read_csv("techpowerup_gpu-specs.csv")
 res=[]
-for idx, row in df.iterrows():
+for idx, row in [(1,df.iloc[347],)]:
     url=(("https://www.techpowerup.com")+(row.url))
-    time.sleep(3)
+    time.sleep((2.0000000298023224e-1))
     print("requesting {} [{}/{}]".format(url, idx, len(df)))
     # https://pythonprogramminglanguage.com/web-scraping-with-pandas-and-beautifulsoup/
     r=requests.get(url)
     content=r.text.replace("&#8203", "")
     soup=BeautifulSoup(content, "html.parser")
-    sections=soup.find_all("section", {("class"):("details")})
-    detail_list=(([("url",row.url,), ("time",time.time(),)])+(functools.reduce(operator.iconcat, list(map(lambda section: list(map(lambda row: (((section.find("h2").text.strip())+(" ")+(row.dt.text.strip())),row.dd.text.strip(),), ((((section.find("div")) and (section.find("div").find_all("dl")))) or ([])))), sections)), [])))
-    res.append(dict(detail_list))
+    sections=soup.find_all(lambda tag: ((((tag.name)==("section"))) and (((tag.get("class"))==(["details"])))))
+    detail_list_=(([("url",row.url,), ("time",time.time(),)])+(functools.reduce(operator.iconcat, list(map(lambda section: list(map(lambda row: (((section.find("h2").text.strip())+(" ")+(row.dt.text.strip())),row.dd.text.strip(),), ((((section.find("div")) and (section.find("div").find_all("dl")))) or ([])))), sections)), [])))
+    columns=[]
+    for section in sections:
+        col_orig=section.h2.text.strip()
+        col=col_orig
+        count=2
+        while ((col in columns)):
+            col=((col_orig)+("_")+(str(count)))
+            count=((1)+(count))
+        columns.append(col)
+    print(columns)
     df_out=pd.DataFrame(res)
-    df_out.to_csv("techpowerup_gpu-specs_details.csv")
+    df_out.to_csv("techpowerup_gpu-specs_details_{}.csv".format(int(time.time())))
